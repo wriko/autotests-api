6579f7d184382cebae78d7235a336ba09b403621
wriko.github.io <40769792+wriko@users.noreply.github.com>
2025-07-01 14:18:43 +0300

    Маркировки Pytest. Практика работы с pytest маркировкам

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,3 +1,5 @@
+import pytest
+
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
 from clients.users.public_users_client import get_public_users_client
@@ -8,8 +10,8 @@ from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
 
-
-
+@pytest.mark.regression
+@pytest.mark.authentication
 def test_login():
     # инициализация клиентов
     public_users_client = get_public_users_client()
