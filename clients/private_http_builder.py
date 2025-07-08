15c54f9facd1d3a8e23be04cb18371ad599fb6ad
wriko.github.io <40769792+wriko@users.noreply.github.com>
2025-07-08 16:02:24 +0300

    Рефакторинг и подготовка к написанию API автотестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,16 +1,23 @@
 from httpx import Client
 from pydantic import BaseModel
+from functools import lru_cache
 from clients.authentication.authentication_client import get_authentication_client
 from clients.authentication.authentication_schema import LoginRequestSchema
 
 
 
-class AuthenticationUserSchema(BaseModel):
+class AuthenticationUserSchema(BaseModel, frozen=True): # Используем frozen=True для создания неизменяемого объекта, что позволяет использовать его в качестве ключа кэша
     email: str
     password: str
 
-
+@lru_cache(maxsize=None) # Кэширование для оптимизации повторного использования клиента для аутентификации и доступа к приватным API
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
+    """
+    Функция создает экземпляр httpx.Client с аутентификацией пользователя.
+
+    :param user: объект AuthenticationUserSchema, содержащий email и password пользователя
+    :return: экземпляр httpx.Client с установленным заголовком Authorization
+    """
     autentication_client = get_authentication_client()
 
     login_request = LoginRequestSchema(email=user.email, password=user.password)
